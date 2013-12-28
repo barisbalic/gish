2b2cda5d114ae8628b07df1ff56a5b892f75afab
Despo Pentara <despo@despo.me>
2013-12-28 20:55:12 +0000

    format comment times

--- lib/gish/presentation/comment.rb
+++ lib/gish/presentation/comment.rb
@@ -1,16 +1,17 @@
 module Gish
   class Comment
     include TerminalHelpers
+    include DateHelpers
 
     def initialize(github_comment)
       @body = github_comment.body.gsub("\r\n", "\n")
       @user = github_comment.user.login
       @id = github_comment.id
-      @created_at = github_comment.created_at
+      @created_at = time_in_words(github_comment.created_at)
     end
 
     def to_s
-      output = "\n#{@id} #{bold(@user)} commented on #{@created_at}"
+      output = "\n#{@id} #{bold(@user)} commented #{@created_at}"
       output << "\n\"#{@body}\"\n"
       output
     end
