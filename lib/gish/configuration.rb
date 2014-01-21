04396cae99eed1c5f47e92a2d660dbd02de4fe9f
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2014-01-21 15:56:50 +0000

    Use personal token for Gish instead of asking for credentials

--- lib/gish/configuration.rb
+++ lib/gish/configuration.rb
@@ -44,25 +44,14 @@ module Gish
       url.match(/:(.*)\.git/)[1] rescue ''
     end
 
-    def retrieve_or_generate_token
+   def retrieve_or_generate_token
       return File.read(@config_file) if File.exists?(@config_file)
 
-      puts "(step 1 of 2) Gish needs your login details to create a personal use token.  This only happens once and your credentials will not be stored."
-      username = prompt('Username: ')
-      password = prompt('Password: ', masked: true)
+      puts 'First time using Gish? Sweet! Please go to https://github.com/settings/applications and create a "Personal Access Token" to continue'
+      token = prompt('Token: ')
 
-      puts "(step 2 of 2) Gish works with public repositories by default, but if you want to use it with We are about to create a token, but first you need to decide if you want to use Gish with private repositories, we need to know now!"
-      private_access = confirm("Use with private repositories? (y/n) ")
-
-      github = Octokit::Client.new(:login => username, :password => password)      
-      options = {
-        :note => 'Gish: Github Issues on the command-line.',
-        :note_url => 'http://github.com/barisbalic/gish/'
-      }
-      options.merge!({:scopes => ['repo']}) if private_access
-      authorization = github.create_authorization(options)
-
-      File.open(@config_file, 'w+') { |file| file.write(authorization.token) }
+      File.open(@config_file, 'w+') { |file| file.write(token) }
     end
+
   end
 end
